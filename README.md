999488ecce077dffeba0c6c0a19c5bf53d476530
AlexanderBeS <oibespalov@gmail.com>
2022-06-06 13:51:17 +0300

    Added mutation

--- README.md
+++ README.md
@@ -7,13 +7,35 @@ node server.js
 ## Make request
 
 ### GraphiQl
-
+```
 { 
     rollDice(numDice: 3, numSides: 6)
 }
 
-### Console
 
+{
+  getDie(numSides: 6) {
+    rollOnce
+    roll(numRolls: 3)
+  }
+}
+
+{ 
+  getMessage(id: "3bea0e86d05c6e91b5d4")
+  {id, author, content}
+}
+
+mutation {
+  createMessage(input: {
+    author: "andy",
+    content: "hope is a good thing",
+  }) {
+    id
+  }
+}
+```
+### Console
+```
 fetch(
 '/graphql', 
     {
@@ -30,3 +52,47 @@ fetch(
 )
 .then(r => r.json())
 .then(data => console.log('data returned:', data));
+```
+
+
+Mutation example
+
+```
+mutation {
+  createMessage(input: {
+    author: "andy",
+    content: "hope is a good thing",
+  }) {
+    id
+  }
+}
+```
+
+```
+var author = 'andy';
+var content = 'hope is a good thing';
+var query = `mutation CreateMessage($input: MessageInput) {
+  createMessage(input: $input) {
+    id
+  }
+}`;
+
+fetch('/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+    variables: {
+      input: {
+        author,
+        content,
+      }
+    }
+  })
+})
+  .then(r => r.json())
+  .then(data => console.log('data returned:', data));
+```
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express');
 var {graphqlHTTP} = require('express-graphql');
 var {buildSchema} = require('graphql');
 const RandomDie = require("./RandomDieClass");
+const Message = require("./MessageClass");
 
+// Maps username to content
+var fakeDatabase = {};
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
     type RandomDie {
@@ -12,17 +15,44 @@ var schema = buildSchema(`
     }
 
     type Query {
-        getDie(numSides: Int): RandomDie,
+        ip: String
+        getDie(numSides: Int): RandomDie
         hello: String
         quoteOfTheDay: String
         random: Float!
         rollDice(numDice: Int!, numSides: Int): [Int]
+        getMessage(id: ID!): Message
+    }
+    
+    input MessageInput {
+        content: String
+        author: String
+    }
+    
+    type Message {
+        id: ID!
+        content: String
+        author: String
+    }
+    
+    type Mutation {
+        createMessage(input: MessageInput): Message
+        updateMessage(id: ID!, input: MessageInput): Message
     }
 `);
 
 
+
+const loggingMiddleware = (req, res, next) => {
+    console.log('ip:', req.ip);
+    next();
+}
+
 // The root provides a resolver function for each API endpoint
 var root = {
+    ip: function (args, request) {
+        return request.ip;
+    },
     getDie: ({numSides}) => {
         return new RandomDie(numSides || 6);
     },
@@ -41,7 +71,29 @@ var root = {
             output.push(1 + Math.floor(Math.random() * (numSides || 6)));
         }
         return output;
-    }
+    },
+
+    getMessage: ({id}) => {
+        if (!fakeDatabase[id]) {
+            throw new Error('no message exists with id ' + id);
+        }
+        return new Message(id, fakeDatabase[id]);
+    },
+    createMessage: ({input}) => {
+        // Create a random id for our "database".
+        var id = require('crypto').randomBytes(10).toString('hex');
+
+        fakeDatabase[id] = input;
+        return new Message(id, input);
+    },
+    updateMessage: ({id, input}) => {
+        if (!fakeDatabase[id]) {
+            throw new Error('no message exists with id ' + id);
+        }
+        // This replaces all old data, but some apps might want partial update.
+        fakeDatabase[id] = input;
+        return new Message(id, input);
+    },
 };
 
 
